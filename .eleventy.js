fb7ad0f3e043f85812ca4525d04610aa2012b2b4
Jacob Barnett <jacob@havenlife.com>
2020-07-23 17:59:18 -0400

    first pass at adding tabs to the main screen

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 module.exports = (config) => {
     config.addPassthroughCopy("src/favicon.ico");
     config.addPassthroughCopy("src/assets/**/*");
+    config.addPassthroughCopy("src/js/*");
     
     config.addPlugin(lazyImagesPlugin, {
         transformImgPath: (imgPath) => { return `src/${imgPath}`}
